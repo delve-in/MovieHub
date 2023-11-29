a47cdad9bcffd437f039be166fce139d4ff9833d
Mohamed Amin A Osman <mo5250760@gmail.com>
2023-11-29 16:13:14 +1100

    Fixed Tailwind Bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ['./views/**/*.handlebars'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
