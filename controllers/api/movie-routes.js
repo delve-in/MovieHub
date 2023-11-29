f20cee4320eca342317ef4ab05106fc30dbd4ba2
rtrevill <139879271+rtrevill@users.noreply.github.com>
2023-11-29 17:32:20 +1100

    added api to find youtube link and imdb rating

--- controllers/api/movie-routes.js
+++ controllers/api/movie-routes.js
@@ -1,17 +1,15 @@
 const router = require('express').Router();
-// const { Comments } = require('../../../../Tech-Blog/MVC-tech-blog/models');
-const { commentRating, Movie, User, wishlist, Comment } = require('../../models');
+const { commentRating, Movie, User, wishlist } = require('../../models');
 const CommentRating = require('../../models/CommentRating');
 const Wishlist = require('../../models/Wishlist');
 
 
 router.get('/:id', async (req, res) => {
     try{
-        // res.send('Hi there')
     const IMDB_id = req.params.id;
-    const url1 = `https://imdb8.p.rapidapi.com/title/get-details?tconst=${IMDB_id}`;
     const url2 =  `https://imdb8.p.rapidapi.com/title/get-overview-details?tconst=${IMDB_id}&currentCountry=AU`;
     const url3 = `https://streaming-availability.p.rapidapi.com/get?output_language=en&imdb_id=${IMDB_id}`;
+    const url4 = `https://movies-tv-shows-database.p.rapidapi.com/?movieid=${IMDB_id}`
     const options = {
 	method: 'GET',
 	headers: {
@@ -19,23 +17,31 @@ router.get('/:id', async (req, res) => {
 		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
         }
     };
-const options2 = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'eb00155c14mshf8e725094566b56p172ca5jsnf5a2489f14e5',
-		'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-        }
-    };
-    // const response1 = await fetch(url1, options);
-    // const result1 = await response1.json();
+    const options2 = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'eb00155c14mshf8e725094566b56p172ca5jsnf5a2489f14e5',
+            'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+            }
+        };
+    const options3 = {
+        method: 'GET',
+        headers: {
+            Type: 'get-movie-details',
+            'X-RapidAPI-Key': 'eb00155c14mshf8e725094566b56p172ca5jsnf5a2489f14e5',
+            'X-RapidAPI-Host': 'movies-tv-shows-database.p.rapidapi.com'
+            }
+        };
+    
     const response2 = await fetch(url2, options);
     const result2 = await response2.json();
     const response3 = await fetch(url3, options2);
     const result3 = await response3.json();
-    // console.log(result3);
-    // console.log(result3.result.streamingInfo, result3.result.title);
+    const response4 = await fetch(url4, options3);
+    const result4 = await response4.json();
 
-
+    const imdb_rating = result4.imdb_rating;
+    const youtubeKey = result4.youtube_trailer_key;
     const allStreaming = result3.result.streamingInfo;
     const cityArray = (Object.keys(allStreaming));
 
@@ -54,25 +60,19 @@ const funfunction = (allStreaming) => {
 };
  funfunction(allStreaming);
  let summarisedServices = [...new Set(streamArray)];
-//  console.log(summarisedServices);
 
 
-const getMovie = await Movie.findOne({where: {IMDB_id: req.params.id}}, {include: [{model: CommentRating}, {model: Wishlist}]});
+const getMovie = await Movie.findOne({where: {IMDB_id: req.params.id}});
 const movieID = getMovie.id;
-const comments = await Movie.findByPk(movieID,{include: [{model: CommentRating}]});
-const plainMovie = comments.get({plain:true});
+const comments = await CommentRating.findAll({where: {movie_id: movieID}, include: [{model: User}]} );
+const movieComments = comments.map((comment) => comment.get({plain:true}));
 
 
-
-    const movieTITLE = result2.title.title;
-    const moviePoster = result2.title.image.url;
-    const movieSynopsis = result2.plotSummary.text;
-    console.log(movieTITLE, moviePoster, movieSynopsis);
-    res.status(200).json({movieTITLE, moviePoster, movieSynopsis, comments, summarisedServices});
-
-
-
-
+const movieTITLE = result2.title.title;
+const moviePoster = result2.title.image.url;
+const movieSynopsis = result2.plotSummary.text;
+console.log(movieTITLE, moviePoster, movieSynopsis);
+res.status(200).json({movieTITLE, moviePoster, movieSynopsis, imdb_rating, youtubeKey, movieComments, summarisedServices});
 
 
     }catch(err){
